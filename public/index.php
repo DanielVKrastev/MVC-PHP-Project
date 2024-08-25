bee0ab1d56efb597cba274d9174caab8531d4b74
Даниел Валентинов <danielkrastev01@gmail.com>
2024-08-25 22:30:26 +0300

    create routing

--- public/index.php
+++ public/index.php
@@ -1,1 +1,28 @@
-hello world
+<?php
+
+session_start();
+
+class App{
+    private function splitURL(){
+        $URL = $_GET['url'] ?? 'home';
+        $URL = explode("/", $URL);
+       
+        return $URL;
+    }
+    
+    private function loadController(){
+        $URL = splitURL();
+    
+        $filename = "../app/controllers/".ucfirst($URL[0]).".php";
+        if(file_exists($filename)){
+            require $filename;
+        }else{
+            $filename = "../app/controllers/_404.php";
+            require $filename;
+        }
+    }
+}
+
+
+
+loadController();
