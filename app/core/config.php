b163ce45028ee4259a885f3b0e5b6f08ed4dea6d
Даниел Валентинов <danielkrastev01@gmail.com>
2024-09-01 23:26:41 +0300

    create function for select, insert, update and delete in Model.php

--- app/core/config.php
+++ app/core/config.php
@@ -2,9 +2,23 @@
 
 if($_SERVER['SERVER_NAME'] == 'localhost'){
 
+    /*Database config */
+    define('DBNAME', 'my_db_mvc');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', '');
+    
     define('ROOT', 'http://localhost/mvc-php-project/public'); //website name
 
 }else{
+
+    /*Database config */
+    define('DBNAME', 'my_db_mvc');
+    define('DBHOST', 'localhost');
+    define('DBUSER', 'root');
+    define('DBPASS', '');
+    define('DBDRIVER', '');
 
     define('ROOT', 'http://www.mywebsite.com'); 
     
