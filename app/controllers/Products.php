4f27fc46e259fc92119313e3ea1ee9539abdb6e1
Даниел Валентинов <danielkrastev01@gmail.com>
2024-09-07 12:31:42 +0300

    important files in app, defined ROOTPATH in index.php

--- app/controllers/Products.php
+++ app/controllers/Products.php
@@ -1,7 +1,11 @@
 <?php
 
-class Products extends Controller
+defined('ROOTPATH') OR exit('Access Denied!');
+
+class Products
 {
+    use Controller;
+    
     public function index()
     {
         $this->view('products');
